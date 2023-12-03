5a30a0b5bc43576562bddb87880f2492ecbd9c2d
Andrew Udvare <audvare@gmail.com>
2023-12-03 06:13:51 -0500

    livecheck/main: fix special_vercmp; invalid versions should go to the end

--- livecheck/main.py
+++ livecheck/main.py
@@ -208,7 +208,7 @@ def get_props(search_dir: str,
 
 
 def special_vercmp(x: str, y: str) -> int:
-    return -1 if (ret := vercmp(x, y)) is None else ret
+    return 1 if (ret := vercmp(x, y)) is None else ret
 
 
 class ExpectedSHALine(RuntimeError):
