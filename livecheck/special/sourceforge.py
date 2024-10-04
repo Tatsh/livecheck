f0589e5ff4e580b96073cc62b0c4fdd66b56293b
INODE64 <web@inode64.com>
2024-10-04 14:58:28 +0200

    Include support for sourceforge too

---
+++ livecheck/special/sourceforge.py
@@ -0,0 +1,29 @@
+import requests
+import re
+
+from loguru import logger
+
+__all__ = ("get_latest_sourceforge_package",)
+
+
+def get_latest_sourceforge_package(program_name: str) -> tuple[str, str]:
+    url = f"https://sourceforge.net/projects/{project_name}/best_release.json"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+
+        release_json = response.json()
+
+        filename = release_json['release']['filename']
+
+        version_match = re.search(r'/(\d+\.\d+\.\d+)/', filename)
+        download_url = f"https://downloads.sourceforge.net/{project_name}/{project_name}-{latest_version}.tar.gz"
+
+        if version_match:
+            version = version_match.group(1)
+            return version, download_url
+
+    except requests.exceptions.HTTPError as e:
+        logger.debug(f"Error accessing the URL: {e}")
+
+    return '', ''
