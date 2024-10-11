68e02a2d706aa301a7572249c26b0b66084a36ac
INODE64 <web@inode64.com>
2024-10-11 14:31:18 +0200

    Include support for jetbrains update version

--- livecheck/special/jetbrains.py
+++ livecheck/special/jetbrains.py
@@ -1,8 +1,12 @@
 import requests
+import tempfile
+import tarfile
+import io
 
+from pathlib import Path
 from loguru import logger
 
-__all__ = ("def get_latest_jetbrains_package",)
+__all__ = ("def get_latest_jetbrains_package", "update_jetbrains_ebuild")
 
 
 # TODO: Support for EAP versions
@@ -40,3 +44,41 @@ def get_latest_jetbrains_package(product_code: str, developer: bool = False) -> 
         logger.debug(f"Error accessing the URL: {e}")
 
     return '', ''
+
+
+def get_first_directory_in_tar_gz(url: str) -> str | None:
+    response = requests.get(url)
+    response.raise_for_status()
+
+    fileobj = io.BytesIO(response.content)
+    with tarfile.open(fileobj=fileobj, mode='r:gz') as tar:
+        for member in tar.getmembers():
+            if member.isdir():
+                return member.name.rstrip('/')
+    return None
+
+
+def update_jetbrains_ebuild(ebuild: str | Path, url: str) -> None:
+    version = get_first_directory_in_tar_gz(url)
+
+    if not version:
+        logger.warning('No version found in the tar.gz file.')
+        return
+
+    version = version.split('-', 1)[-1]
+
+    ebuild = Path(ebuild)
+    tf = tempfile.NamedTemporaryFile(mode='w',
+                                     prefix=ebuild.stem,
+                                     suffix=ebuild.suffix,
+                                     delete=False,
+                                     dir=ebuild.parent)
+    with ebuild.open('r') as f:
+        for line in f.readlines():
+            if line.startswith('MY_PV='):
+                logger.debug('Found MY_PV= line.')
+                tf.write(f'MY_PV="{version}"\n')
+            else:
+                tf.write(line)
+    ebuild.unlink()
+    Path(tf.name).rename(ebuild).chmod(0o0644)
