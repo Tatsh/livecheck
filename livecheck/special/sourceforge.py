73bf01d11a507d7b0d2e52bfdf6aa296f38b39be
INODE64 <web@inode64.com>
2024-10-17 09:15:50 +0200

    Optimize code and use URL for download file

--- livecheck/special/sourceforge.py
+++ livecheck/special/sourceforge.py
@@ -1,7 +1,6 @@
 import requests
 import re
 
-from http import HTTPStatus
 from loguru import logger
 
 __all__ = ("get_latest_sourceforge_package",)
@@ -12,24 +11,25 @@ def get_latest_sourceforge_package(project_name: str) -> tuple[str, str]:
     try:
         response = requests.get(url)
         response.raise_for_status()
-        if response.status_code == HTTPStatus.OK:
-            release_json = response.json()
+        release_json = response.json()
 
-            release = release_json.get('release')
-            if release and release.get('filename'):
-                filename = release['filename']
-                version_match = re.search(r'/(\d+\.\d+\.\d+)/', filename)
+        release = release_json.get('release')
+        if release and release.get('filename'):
+            filename = release['filename']
+            version_match = re.search(r'/(\d+(?:\.\d+)+)/', filename)
 
-                if version_match:
-                    version = version_match.group(1)
-                    download_url = f"https://downloads.sourceforge.net/{project_name}/{project_name}-{version}.tar.gz"
-                    return version, download_url
+            if version_match:
+                version = version_match.group(1)
+                download_url = release.get('url').rstrip('/download')
+                return version, download_url
         else:
-            logger.debug(f"Project not found: {url}")
+            logger.debug(f"Could not extract filename o release: {url}")
 
     except requests.exceptions.JSONDecodeError as e:
         logger.debug(f"Error decoding JSON {url}: {e}")
     except requests.exceptions.HTTPError as e:
         logger.debug(f"Error accessing the URL: {e}")
+    except Exception as e:
+        logger.debug(f"An unexpected error occurred: {e}")
 
     return '', ''
