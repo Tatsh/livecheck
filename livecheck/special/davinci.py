9cd0d9427ace8586ea354b6ada72baaaa81431e3
Fco Javier Felix <ffelix@inode64.com>
2025-01-06 19:12:56 +0100

    use requests

--- livecheck/special/davinci.py
+++ livecheck/special/davinci.py
@@ -1,6 +1,5 @@
 import json
-import urllib.request
-import urllib.error
+import requests
 from loguru import logger
 
 __all__ = ("get_latest_davinci_package")
@@ -10,16 +9,12 @@ def get_latest_davinci_package(pkg: str) -> str:
     api_url = f"https://www.blackmagicdesign.com/api/support/latest-stable-version/{pkg}/linux"
 
     try:
-        with urllib.request.urlopen(api_url) as response:
-            if response.status != 200:
-                logger.error(f"Request failed with status code: {response.status}")
-            else:
-                data = json.load(response)
-                return f"{data['linux']['major']}.{data['linux']['minor']}.{data['linux']['releaseNum']}"
-    except urllib.error.HTTPError as e:
-        logger.error(f"HTTP error: {e.code} {e.reason}")
-    except urllib.error.URLError as e:
-        logger.error(f"URL error: {e.reason}")
+        r = requests.get(api_url, timeout=10)
+        r.raise_for_status()
+        data = r.json()
+        return f"{data['linux']['major']}.{data['linux']['minor']}.{data['linux']['releaseNum']}"
+    except requests.exceptions.HTTPError as e:
+        logger.error(f"URL error: {e}")
     except (json.JSONDecodeError, KeyError):
         logger.error("Error parsing JSON or missing required fields")
     return ''
