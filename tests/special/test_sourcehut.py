0c24c0f39e7a9d20fa495fd5dd7b8d1b5e8a9195
INODE64 <web@inode64.com>
2025-02-07 14:11:09 +0100

    fix mypy test

--- tests/special/test_sourcehut.py
+++ tests/special/test_sourcehut.py
@@ -1,79 +1,55 @@
 from livecheck.settings import LivecheckSettings
 from livecheck.special.sourcehut import extract_owner_repo, get_branch
 
-ebuild = "app-portage/livecheck-1.0"
-settings = LivecheckSettings(branches={},
-                             checksum_livechecks=None,
-                             custom_livechecks=None,
-                             dotnet_projects=None,
-                             go_sum_uri=None,
-                             type_packages=None,
-                             no_auto_update=None,
-                             semver=None,
-                             sha_sources=None,
-                             transformations=None,
-                             yarn_base_packages=None,
-                             yarn_packages=None,
-                             jetbrains_packages=None,
-                             keep_old=None,
-                             gomodule_packages=None,
-                             gomodule_path=None,
-                             nodejs_packages=None,
-                             nodejs_path=None,
-                             development=None,
-                             composer_packages=None,
-                             composer_path=None,
-                             regex_version=None,
-                             restrict_version=None,
-                             sync_version=None,
-                             stable_version=None)
+EBUILD = "app-portage/livecheck-1.0"
+settings = LivecheckSettings()
 
 
-def test_extract_owner_repo_valid_git_url():
+def test_extract_owner_repo_valid_git_url() -> None:
     url = "https://git.sr.ht/~owner/repo"
     expected = ("git.sr.ht", "~owner", "repo")
     assert extract_owner_repo(url) == expected
 
 
-def test_extract_owner_repo_valid_hg_url():
+def test_extract_owner_repo_valid_hg_url() -> None:
     url = "https://hg.sr.ht/~owner/repo"
     expected = ("hg.sr.ht", "~owner", "repo")
     assert extract_owner_repo(url) == expected
 
 
-def test_extract_owner_repo_invalid_url():
+def test_extract_owner_repo_invalid_url() -> None:
     url = "https://example.com/~owner/repo"
     expected = ("", "", "")
     assert extract_owner_repo(url) == expected
 
 
-def test_extract_owner_repo_missing_owner_repo():
+def test_extract_owner_repo_missing_owner_repo() -> None:
     url = "https://git.sr.ht/"
     expected = ("", "", "")
     assert extract_owner_repo(url) == expected
 
 
-def test_extract_owner_repo_no_protocol():
+def test_extract_owner_repo_no_protocol() -> None:
     url = "git.sr.ht/~owner/repo/log/master/rss.xml"
     expected = ("git.sr.ht", "~owner", "repo")
     assert extract_owner_repo(url) == expected
 
 
-def test_get_branch_from_url():
+def test_get_branch_from_url() -> None:
     url = "https://git.sr.ht/~owner/repo/log/master/rss.xml"
-    assert get_branch(url, ebuild, settings) == "master"
+    assert get_branch(url, EBUILD, settings) == "master"
 
 
-def test_get_branch_from_settings():
+def test_get_branch_from_settings() -> None:
     url = "https://git.sr.ht/~owner/repo/log/develop/rss.xml"
-    assert get_branch(url, ebuild, settings) == "develop"
+    assert get_branch(url, EBUILD, settings) == "develop"
 
 
-def test_get_branch_default_master():
+def test_get_branch_default_master() -> None:
     url = "https://git.sr.ht/~owner/repo/log/master/rss.xml"
-    assert get_branch(url, ebuild, settings) == "master"
+    assert get_branch(url, EBUILD, settings) == "master"
 
 
-def test_get_branch_invalid_url():
+def test_get_branch_invalid_url() -> None:
     url = "https://example.com/~owner/repo"
-    assert get_branch(url, ebuild, settings) == ""
+    assert get_branch(url, EBUILD, settings) == ""
