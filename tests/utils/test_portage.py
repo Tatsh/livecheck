b924cc11a0e091bddbf32317803ada59ff6344ae
Fco Javier Felix <ffelix@inode64.com>
2025-02-15 11:41:35 +0100

    support for a0 / b0 -> alpha / beta

--- tests/utils/test_portage.py
+++ tests/utils/test_portage.py
@@ -27,6 +27,7 @@ from livecheck.utils.portage import sanitize_version
                           ("v1.12.post318", "1.12_p318"), ("1.002", "1.002"),
                           ("1.3.0-build.4", "1.3.0"), ("0.2.tar.gz", "0.2"),
                           ("0.8.1-pl5", "0.8.1_p5"), ("0.8 patchlevel   6", "0.8_p6"),
-                          ("0.0.8b2", "0.0.8_beta2"), ("0.0.8a5", "0.0.8_alpha5")])
+                          ("0.0.8b2", "0.0.8_beta2"), ("0.0.8a5", "0.0.8_alpha5"),
+                          ("0.1.8b0", "0.1.8_beta")])
 def test_sanitize_version(version: str, expected: str) -> None:
     assert sanitize_version(version) == expected
