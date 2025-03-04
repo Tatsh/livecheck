d1cbce2988bf52737089858d818cae29338d1fa9
INODE64 <web@inode64.com>
2025-03-04 16:00:01 +0100

    new test

--- tests/special/test_gitlab.py
+++ tests/special/test_gitlab.py
@@ -1,20 +1,63 @@
+from typing import Any
+
 import pytest
 
-from livecheck.special.gitlab import extract_domain_and_namespace
+from livecheck.special.gitlab import extract_domain_and_namespace, is_gitlab
+
+test_cases = {
+    "valid_gitlab_url": {
+        "url": "https://gitlab.com/group/project",
+        "expected": ("gitlab.com", "group/project", "project"),
+        "is_gitlab": True
+    },
+    "invalid_gitlab_url": {
+        "url": "https://notgitlab.com/group/project",
+        "expected": ("", "", ""),
+        "is_gitlab": False
+    },
+    "gitlab_es_url": {
+        "url": "https://gitlab.es/group/project",
+        "expected": ("", "", ""),
+        "is_gitlab": False
+    },
+    "example_gitlab_url": {
+        "url": "https://example.gitlab.com/group/project",
+        "expected": ("", "", ""),
+        "is_gitlab": False
+    },
+    "gitlab_example_url": {
+        "url": "https://gitlab.example.com/group/project",
+        "expected": ("gitlab.example.com", "group/project", "project"),
+        "is_gitlab": True
+    },
+    "example_com_url": {
+        "url": "https://example.com/group/project",
+        "expected": ("", "", ""),
+        "is_gitlab": False
+    },
+    "gitlab_merge_request_url": {
+        "url": "https://gitlab.com/group/project/-/merge_requests",
+        "expected": ("gitlab.com", "group/project", "project"),
+        "is_gitlab": True
+    },
+    "gitlab_subgroup_url": {
+        "url": "https://gitlab.com/group/subgroup/project",
+        "expected": ("gitlab.com", "group/subgroup/project", "project"),
+        "is_gitlab": True
+    },
+    "manjaro": {
+        "url": "https://gitlab.manjaro.org/fhdk/udev-usb-sync",
+        "expected": ("gitlab.manjaro.org", "fhdk/udev-usb-sync", "udev-usb-sync"),
+        "is_gitlab": True
+    }
+}
 
 
-@pytest.mark.parametrize(
-    ("url", "expected"),
-    [("https://gitlab.com/group/project", ("gitlab.com", "group/project", "project")),
-     ("https://notgitlab.com/group/project", ("", "", "")),
-     ("https://gitlab.es/group/project", ("", "", "")),
-     ("https://example.gitlab.com/group/project", ("", "", "")),
-     ("https://gitlab.example.com/group/project",
-      ("gitlab.example.com", "group/project", "project")),
-     ("https://example.com/group/project", ("", "", "")),
-     ("https://gitlab.com/group/project/-/merge_requests",
-      ("gitlab.com", "group/project", "project")),
-     ("https://gitlab.com/group/subgroup/project",
-      ("gitlab.com", "group/subgroup/project", "project"))])
-def test_extract_domain_and_namespace(url: str, expected: tuple[str, str, str]) -> None:
-    assert extract_domain_and_namespace(url) == expected
+@pytest.mark.parametrize("test_case", test_cases.values(), ids=test_cases.keys())
+def test_extract_domain_and_namespace(test_case: dict[str, Any]) -> None:
+    assert extract_domain_and_namespace(test_case["url"]) == test_case["expected"]
+
+
+@pytest.mark.parametrize("test_case", test_cases.values(), ids=test_cases.keys())
+def test_is_gitlab(test_case: dict[str, Any]) -> None:
+    assert is_gitlab(test_case['url']) == test_case["is_gitlab"]
